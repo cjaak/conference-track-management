03e033097a144f23df1087a8ebb21d723825468d
charlie <charlie.oliver.wiegand@gmail.com>
2022-02-16 18:10:11 +0100

    feat: implemented first case in validate talk

--- ConferenceTrackManagement/ConferenceTrackManagement/InputHandler.cs
+++ ConferenceTrackManagement/ConferenceTrackManagement/InputHandler.cs
@@ -1,6 +1,23 @@
+using System.Text.RegularExpressions;
+
 namespace ConferenceTrackManagement;
 
 public class InputHandler
 {
-    
+    public string FilePath { get; }
+
+    public InputHandler(string filePath)
+    {
+        FilePath = filePath;
+    }
+
+    internal bool ValidateTalk(string talk)
+    {
+        Regex regex = new Regex("([a-zA-Z]+( [a-zA-Z]+)+) [0-9]+min", RegexOptions.IgnoreCase);
+        if (regex.IsMatch(talk))
+        {
+            return true;
+        }
+        return false;
+    }
 }
