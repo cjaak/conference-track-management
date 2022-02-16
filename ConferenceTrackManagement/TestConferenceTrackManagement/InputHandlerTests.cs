eea74d272e5fe6238979a034618ccbad27f4082a
charlie <charlie.oliver.wiegand@gmail.com>
2022-02-16 18:20:58 +0100

    test: validate title lightning returns true

--- ConferenceTrackManagement/TestConferenceTrackManagement/InputHandlerTests.cs
+++ ConferenceTrackManagement/TestConferenceTrackManagement/InputHandlerTests.cs
@@ -10,16 +10,31 @@ public class InputHandlerTests
     {
         InputHandler handler = new InputHandler("");
         string input = "dummy title 30min";
+        
         bool output = handler.ValidateTalk(input);
+        
         Assert.That(output, Is.True);
     }
 
     [Test]
-    public void ValidateTalk_Title_With_Numbers_ReturnsFalse()
+    public void ValidateTalk_TitleWithNumbers_ReturnsFalse()
     {
         InputHandler handler = new InputHandler("");
         string input = "dummy 123 title 30min";
+        
         bool output = handler.ValidateTalk(input);
+        
         Assert.That(output, Is.False);
+    }
+
+    [Test]
+    public void ValidateTalk_TitleLightning_ReturnsTrue()
+    {
+        InputHandler handler = new InputHandler("");
+        string input = "dummy title lightning";
+        
+        bool output = handler.ValidateTalk(input);
+        
+        Assert.That(output, Is.True);
     }
 }
