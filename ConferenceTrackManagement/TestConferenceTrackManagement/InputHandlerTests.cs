1ddeb608c143e25b8d1a839eff0877e9ff0ff22f
charlie <charlie.oliver.wiegand@gmail.com>
2022-02-16 18:15:58 +0100

    test: validate title title with numbers returns false

--- ConferenceTrackManagement/TestConferenceTrackManagement/InputHandlerTests.cs
+++ ConferenceTrackManagement/TestConferenceTrackManagement/InputHandlerTests.cs
@@ -8,8 +8,18 @@ public class InputHandlerTests
     [Test]
     public void ValidateTalk_Title30min_ReturnsTrue()
     {
+        InputHandler handler = new InputHandler("");
         string input = "dummy title 30min";
-        bool output = InputHandler.ValidateTalk(input);
+        bool output = handler.ValidateTalk(input);
         Assert.That(output, Is.True);
+    }
+
+    [Test]
+    public void ValidateTalk_Title_With_Numbers_ReturnsFalse()
+    {
+        InputHandler handler = new InputHandler("");
+        string input = "dummy 123 title 30min";
+        bool output = handler.ValidateTalk(input);
+        Assert.That(output, Is.False);
     }
 }
