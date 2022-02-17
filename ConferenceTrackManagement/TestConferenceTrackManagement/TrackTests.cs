e9f98da917c349c0239bb18637f19d737853178d
charlie <charlie.oliver.wiegand@gmail.com>
2022-02-17 17:56:10 +0100

    test: fill tracks complete asserts

--- ConferenceTrackManagement/TestConferenceTrackManagement/TrackTests.cs
+++ ConferenceTrackManagement/TestConferenceTrackManagement/TrackTests.cs
@@ -14,15 +14,44 @@ public class TrackTests
         Track track = new Track(180, 240);
         List<Talk> input = new List<Talk>()
         {
-            new Talk("Foo", 100),
-            new Talk("bar", 100),
-            new Talk("foo foo", 70),
-            new Talk("Foo Bar", 80),
-            new Talk("bar bar", 30),
-            new Talk("Not Used", 50),
-            new Talk("bar foo", 40)
+            new Talk("First", 100),
+            new Talk("Second", 100),
+            new Talk("Third", 70),
+            new Talk("Fourth", 80),
+            new Talk("Fifth", 30),
+            new Talk("Sixth", 100),
+            new Talk("Seventh", 50),
+            new Talk("Eighth", 40)
+        };
+
+        List<Talk> expectedReturn = new List<Talk>()
+        {
+            new Talk("Seventh", 50)
+        };
+
+        List<Talk> expectedMorningList = new List<Talk>()
+        {
+            new Talk("First", 100),
+            new Talk("Second", 100),
+            new Talk("Fourth", 80),
         };
         
+        List<Talk> expectedAfternoonList = new List<Talk>()
+        {
+            new Talk("Third", 70),
+            new Talk("Fifth", 30),
+            new Talk("Sixth", 100),
+            new Talk("Eighth", 40)
+        };
+
+        List<Talk> output = track.FillTracks(input);
+
+        Assert.That(track.MorningSession.RestMinutes, Is.Zero);
+        Assert.That(track.AfternoonSession.RestMinutes, Is.Zero);
+
+        Assert.That(track.MorningSession.TalksInSession, Is.EquivalentTo(expectedMorningList));
+        Assert.That(track.AfternoonSession.TalksInSession, Is.EquivalentTo(expectedAfternoonList));
         
+        Assert.That(output, Is.EquivalentTo(expectedReturn));
     }
 }
