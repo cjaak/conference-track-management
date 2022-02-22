c7b0bc25b9e458ac8b892f538805de768b4e1e88
charlie <charlie.oliver.wiegand@gmail.com>
2022-02-22 17:40:04 +0100

    test: complete timetable printing

--- ConferenceTrackManagement/TestConferenceTrackManagement/ConferenceManagerTests.cs
+++ ConferenceTrackManagement/TestConferenceTrackManagement/ConferenceManagerTests.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Threading.Channels;
 using ConferenceTrackManagement;
 using NUnit.Framework;
 
@@ -26,7 +28,41 @@ public class ConferenceManagerTests
         
         manager.ScheduleConference(input, 100, 200);
 
-        Assert.That(manager.tracks.Count, Is.EqualTo(2));
+        Assert.That(manager.Tracks.Count, Is.EqualTo(2));
 
+    }
+
+    [Test]
+    public void PrintConferenceTimeTable_ListOfTracks_FormattedList()
+    {
+        ConferenceManager manager = new ConferenceManager();
+        Track track1 = new Track(180, 240);
+        Track track2 = new Track(180, 240);
+        
+        track1.MorningSession.TalksInSession.Add(new Talk("Sample A", 180));
+        track1.AfternoonSession.TalksInSession.Add(new Talk("Sample B", 240));
+        track1.AfternoonSession.RestMinutes = new TimeSpan(0);
+        
+        track2.MorningSession.TalksInSession.Add(new Talk("Sample C", 180));
+        track2.AfternoonSession.TalksInSession.Add(new Talk("Sample B", 120));
+        track2.AfternoonSession.RestMinutes = new TimeSpan(0,120,0);
+
+        List<string> expected = new List<string>()
+        {
+            "Track 1:",
+            "09:00 AM Sample A 180min",
+            "12:00 PM Lunch",
+            "01:00 PM Sample B 240min",
+            "05:00 PM Networking Event",
+            "Track 2:",
+            "09:00 AM Sample A 180min",
+            "12:00 PM Lunch",
+            "01:00 PM Sample B 120min",
+            "04:00 PM Networking Event",
+        };
+
+        List<string> output = manager.PrintConferenceTimeTable();
+
+        Assert.That(output, Is.EqualTo(expected));
     }
 }
