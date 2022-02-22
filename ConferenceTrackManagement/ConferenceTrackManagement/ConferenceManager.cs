807e1f79c11d8476f3f77460f66f22966e291256
charlie <charlie.oliver.wiegand@gmail.com>
2022-02-22 18:20:49 +0100

    feat: implement whole timetable printing logic

--- ConferenceTrackManagement/ConferenceTrackManagement/ConferenceManager.cs
+++ ConferenceTrackManagement/ConferenceTrackManagement/ConferenceManager.cs
@@ -2,11 +2,11 @@ namespace ConferenceTrackManagement;
 
 public class ConferenceManager
 {
-    public List<Track> tracks {get;}
+    public List<Track> Tracks {get;}
 
     public ConferenceManager()
     {
-        tracks = new List<Track>();
+        Tracks = new List<Track>();
     }
 
     public void ScheduleConference(List<Talk> talks, int morningMinutes, int afternoonMinutes)
@@ -15,7 +15,25 @@ public class ConferenceManager
         {
             Track track = new Track(morningMinutes, afternoonMinutes);
             talks = track.FillTrack(talks);
-            tracks.Add(track);
+            Tracks.Add(track);
         } while (talks.Count > 0);
+    }
+
+    public List<string> PrintConferenceTimeTable()
+    {
+        var dateNow = DateTime.Now;
+        var morningStart = new DateTime(dateNow.Year, dateNow.Month, dateNow.Day, 9, 0, 0);
+        var afternoonStart = new DateTime(dateNow.Year, dateNow.Month, dateNow.Day, 13, 0, 0);
+        var lunchStart = new DateTime(dateNow.Year, dateNow.Month, dateNow.Day, 12, 0, 0);
+        TimeSpan networkingWaitTime = new TimeSpan(0, 180, 0);
+        List<string> formattedTable = new List<string>();
+        for (int i = 0; i < Tracks.Count; i++)
+        {
+            string title = $"Track {i + 1}:";
+            formattedTable.Add(title);
+            List<string> formattedTrack = Tracks[i].CreatePrintableTrackTimeTable(morningStart, afternoonStart, lunchStart, networkingWaitTime);
+            formattedTable.AddRange(formattedTrack);
+        }
+        return formattedTable;
     }
 }
